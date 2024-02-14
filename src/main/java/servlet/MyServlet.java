eb6c7df006c6aff98f5fd0a12f551183a1fd347b
Alhaj Remi <remi.alhaj@etudiant.univ-rennes1.fr>
2024-02-14 16:20:03 +0100

    servlet

---
+++ src/main/java/servlet/MyServlet.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(name="mytest", urlPatterns={"/myurl"})
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        PrintWriter p = new PrintWriter(resp.getOutputStream());
+        p.print("Hello world");
+        p.flush();
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        super.doPost(req, resp);
+    }
+}
